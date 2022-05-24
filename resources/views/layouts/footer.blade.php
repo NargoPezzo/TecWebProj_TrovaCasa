2bad12ae688e0597f0223ea8e21394ab3a839a68
tomma <tomma@host.docker.internal>
2022-05-24 16:12:54 +0200

    madre mia git

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,30 +1,30 @@
 
         <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="first-item">
+            
+                
+                <center><div class="first-item">
                         <div class="logo">
                             <img src="assets/images/white-logo.png" alt="hexashop ecommerce templatemo">
                         </div>
                         <ul>
-                            <li><a href="#">16501 Collins Ave, Sunny Isles Beach, FL 33160, United States</a></li>
-                            <li><a href="#">hexashop@company.com</a></li>
-                            <li><a href="#">010-020-0340</a></li>
+                            <li><a href="#">Via Brecce Bianche 138, 60131, Ancona, Italia</a></li>
+                            <li><a href="#">info@trovacasa.it</a></li>
+                            <li><a href="#">+39 333 4455667</a></li> 
                         </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3">
+                    </div></center>
+                
+                <!-- <div class="col-lg-3">
                     <h4>Shopping &amp; Categories</h4>
                     <ul>
                         <li><a href="#">Men’s Shopping</a></li>
                         <li><a href="#">Women’s Shopping</a></li>
                         <li><a href="#">Kid's Shopping</a></li>
                     </ul>
-                </div>
+                </div> 
                 <div class="col-lg-3">
-                    <h4>Useful Links</h4>
+                    <h4>Link </h4>
                     <ul>
-                        <li><a href="#">Homepage</a></li>
+                        <li><a href="#">Home</a></li>
                         <li><a href="#">About Us</a></li>
                         <li><a href="#">Help</a></li>
                         <li><a href="#">Contact Us</a></li>
@@ -38,12 +38,12 @@
                         <li><a href="#">Shipping</a></li>
                         <li><a href="#">Tracking ID</a></li>
                     </ul>
-                </div>
-                <div class="col-lg-12">
+                </div> -->
+                <div class="col-lg-12"> *\
                     <div class="under-footer">
-                        <p>Copyright © 2022 HexaShop Co., Ltd. All Rights Reserved. 
+                        <p>Copyright © 2022 TrovaCasa Co., Ltd. 
                         
-                        <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a></p>
+                        <br>Tutti i diritti riservati</p>
                         <ul>
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
