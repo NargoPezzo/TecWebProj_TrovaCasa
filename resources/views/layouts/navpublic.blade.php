a71b75eb7ef251548794a019c83cf90945f79967
Andrea Visi <andreavisi1@gmail.com>
2022-05-26 18:36:34 +0200

    login locatario

--- resources/views/layouts/navpublic.blade.php
+++ resources/views/layouts/navpublic.blade.php
@@ -8,13 +8,13 @@
 <!-- ***** Navbar Start ***** -->
 <ul class='nav'>
     @can('isAdmin')
-        <li><a href="{{ route('chisiamo') }}" class="highlight" title="Home Admin">Home Admin</a></li>
+        <li><a href="{{ route('chisiamo') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Admin</a></li>
     @endcan
     @can('isLocatore')
-        <li><a href="{{ route('chisiamo') }}" class="highlight" title="Home Locatore">Home Locatore</a></li>
+        <li><a href="{{ route('chisiamo') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Locatore</a></li>
     @endcan
     @can('isLocatario')
-        <li><a href="{{ route('homelocatario') }}" class="highlight" title="Home Locatario">Home Locatario</a></li>
+        <li><a href="{{ route('homelocatario') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Locatario</a></li>
     @endcan
     <li><a href="{{ route('offerte') }}" title="Il nostro catalogo">Offerte</a></li>
     <li><a href="{{ route('chisiamo') }}" title="La nostra azienda">Chi Siamo</a></li>
