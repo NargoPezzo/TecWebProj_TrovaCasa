3083a2bb239a9eb4f96a2d4d0097398ab370ad6f
Alex Pugnaloni <s1092591@studenti.univpm.it>
2022-05-30 17:41:39 +0200

    area riservata

--- resources/views/layouts/nav_locatore.blade.php
+++ resources/views/layouts/nav_locatore.blade.php
@@ -8,18 +8,35 @@
 <!-- ***** Navbar Start ***** -->
 <ul class='nav'>
     <li><a href="{{ route('locatore') }}" title="Va alla Home del Locatore">Torna alla home</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Lista delle richieste ricevute">Visualizza richieste</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Inserisci nuovo alloggio">Inserisci alloggio</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Modifica i tuoi alloggi">Modifica alloggi</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Elimina i tuoi alloggi">Elimina alloggi</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Va alle chat">Sezione messaggistica</a></li>
-    <li><a href="{{ route('chisiamo') }}" title="Modificare dati personali">Modifica dati utente</a></li>
-        @auth
-        <li><a href="" class="highlight" title="Esci dal sito" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
-    @endauth
+    
+    
+                            <li class="submenu">
+                                <a href="javascript:;">Alloggio</a>
+                                <ul>
+                                    <li><a href="{{ route('chisiamo') }}" title="Inserisci un alloggio">Inserisci Alloggio</a></li>
+                                    <li><a href="{{ route('chisiamo') }}" title="Modifica un alloggio">Modifica Alloggio</a></li>
+                                    <li><a href="{{ route('chisiamo') }}" title="Elimina un alloggio">Elimina Alloggio</a></li>
+                                    
+                                </ul>
+                            </li>
+                            
+                            <li class="submenu">
+                                <a href="javascript:;">Account</a>
+                                <ul>
+                                    <li><a href="{{ route('chisiamo') }}" title="Lista delle richieste ricevute">Visualizza Richieste</a></li>
+                                    <li><a href="{{ route('chisiamo') }}" title="Modificare dati personali">Sezione messaggistica</a></li>
+                                    <li><a href="{{ route('chisiamo') }}" title="Modificare dati personali">Modifica dati utente</a></li>
+                                    @auth
+                                    <li><a href="" class="highlight" title="Esci dal sito" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                        </form>
+                                @endauth
+                                </ul>
+                            </li>
+    
+    
+        
 </ul>
 
 <!-- ***** INSERIRE ALTRI COMANDI NAVBAR PER VARI TIPI DI UTENTI ***** -->
