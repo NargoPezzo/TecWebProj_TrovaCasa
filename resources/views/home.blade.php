44355616caf278938983093d6491c1a4b63ea4d3
tomma <tomma@host.docker.internal>
2022-06-01 16:18:40 +0200

    working on homepage fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,32 @@
 
     <link rel="stylesheet" href="assets/css/lightbox.css">
 
-    @include('layouts/slideshow')
+    <style>
+    body {font-family:Arial, Helvetica, sans-serif; font-size:12px;}
+    
+            
+    .fadein { 
+        position:relative; height:700px; width:700px; margin:0 auto;
+        
+        padding: 10px;
+    }
+    
+    .fadein img{
+        position:absolute;
+        width: calc(96%);
+        height: calc(94%);
+        object-fit: scale-down;
+    }
+
+</style>
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>
+    $(function(){
+            $('.fadein img:gt(0)').hide();
+            setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+    });
+</script>
     
     </head>
     
