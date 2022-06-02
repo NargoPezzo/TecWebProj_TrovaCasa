c5b530208ccdc2014c1b1eb1a537b56cf08bce4e
tomma <tomma@host.docker.internal>
2022-06-02 15:47:57 +0200

    navbar rework

--- resources/views/layouts/navpublic.blade.php
+++ resources/views/layouts/navpublic.blade.php
@@ -7,30 +7,70 @@
 
 <!-- ***** Navbar Start ***** -->
 <ul class='nav'>
-    @can('isAdmin')
-        <li><a href="{{ route('homeadmin') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Admin</a></li>
-    @endcan
-    @can('isLocatore')
-        <li><a href="{{ route('homelocatore') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Locatore</a></li>
-    @endcan
-    @can('isLocatario')
-        <li><a href="{{ route('homelocatario') }}" class="highlight" title="Sfrutta le tue funzionalità">Home Locatario</a></li>
-    @endcan
     <li><a href="{{ route('offerte') }}" title="Il nostro catalogo">Offerte</a></li>
     <li><a href="{{ route('chisiamo') }}" title="La nostra azienda">Chi Siamo</a></li>
     <li><a href="{{ route('dovesiamo') }}" title="Dove potete trovarci">Dove Siamo</a></li>
     <li><a href="mailto:info@trovacasa.it" title="Scrivici pure">Contattaci</a></li>
-    <li><a href="{{ route('faq') }}" title="Risposte alle domande più comuni">F.A.Q.</a></li>
     <li><a href="{{ route('condizioni') }}" title="Condizioni generali d' uso">Condizioni</a></li>
-    @auth
-        <li><a href="" title="Esci dal sito" class="highlight" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
-    @endauth  
+    
+    @cannot('isAdmin')
+    <li><a href="{{ route('faq') }}" title="Risposte alle domande più comuni">F.A.Q.</a></li>
+    @endcan
+    
+    @can('isAdmin')
+    <li><a href="{{ route('chisiamo') }}" title="Visualizza le statistiche">Statistiche</a></li>
+    <li class="submenu">
+        <a href="javascript:;">F.A.Q</a>
+            <ul>
+                <li><a href="{{ route('inseriscifaq') }}" title="Inserisci nuova FAQ">Inserisci FAQ</a></li>
+                <li><a href="{{ route('chisiamo') }}" title="Modifica le FAQ">Modifica FAQ</a></li>
+                <li><a href="{{ route('chisiamo') }}" title="Elimina le FAQ">Elimina FAQ</a></li>
+            </ul>
+    </li>
+   
+    @endcan
+    
     @guest
         <li><a href="{{ route('login') }}" title="Iscriviti o Accedi">Login / Registrati</a></li>
     @endguest
+    
+    @can('isLocatore')
+        <li class="submenu">
+            <a href="javascript:;">Alloggio</a>
+            <ul>
+                <li><a href="{{ route('inseriscialloggio') }}" title="Inserisci un alloggio">Inserisci Alloggio</a></li>
+                <li><a href="{{ route('chisiamo') }}" title="Modifica un alloggio">Modifica Alloggio</a></li>
+                <li><a href="{{ route('chisiamo') }}" title="Elimina un alloggio">Elimina Alloggio</a></li>
+            </ul>
+        </li>
+    @endcan    
+    
+    @auth
+        <li class="submenu">
+            <a href="javascript:;">Account</a>
+                <ul>
+                    @can('isLocatore')
+                    <li><a href="{{ route('chisiamo') }}" title="Lista delle richieste ricevute">Visualizza Richieste</a></li>
+                    <li><a href="{{ route('modificalocatore') }}" title="Modificare dati personali">Modifica dati utente</a></li>
+                    @endcan
+                    
+                    @can('isLocatario')
+                    <li><a href="{{ route('modificalocatore') }}" title="Modificare dati personali">Modifica dati utente</a></li>
+                    @endcan
+                    
+                    @cannot('isAdmin')
+                    <li><a href="{{ route('chisiamo') }}" title="Va alle chat">Sezione messaggistica</a></li>
+                    @endcan
+                    
+                    <li><a href="" class="highlight" title="Esci dal sito" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                    </form>
+                    
+                                    
+                </ul>
+        </li>
+    @endauth
 </ul>
 
 <!-- ***** Navbar End ***** -->
