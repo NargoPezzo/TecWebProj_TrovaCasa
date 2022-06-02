4b2ece1e928c2260e7fad16224f40cef37f18f7c
Andrea Visi <andreavisi1@gmail.com>
2022-06-02 12:09:59 +0200

    inserisci faq

--- resources/views/faqs/inseriscifaq.blade.php
+++ resources/views/faqs/inseriscifaq.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title>TrovaCasa.it - @yield('title', 'Inserisci Alloggio')</title>
+    <title>TrovaCasa.it - @yield('title', 'Inserisci Faq')</title>
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
