4fa14c33bb78a5fafb2c25bff241625a32bda4e5
tomma <tomma@host.docker.internal>
2022-06-08 16:20:30 +0200

    styling chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,32 +1,70 @@
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <script src="https://kit.fontawesome.com/ea82011960.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/accountstyle.css') }}">
-   
-</head>
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>TrovaCasa.it - @yield('title', 'Visualizza singola offerta')</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/templatemo-hexashop.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/owl-carousel.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css')}}">
+    <style>
+        ricevuto {
+            border-style: solid;
+            border-color: blue;
+            
+        }
+        
+        inviato {
+            border-style: solid;
+            border-color: green;
+            
+        }
+    </style>
+    
+    </head>
 
 <body>
 @isset($messaggi)
 
-
-            
+<div class="about-us">
+    <div class="right-content">
+           
             @foreach($messaggi as $messaggio)  
                 
                 @if($messaggio->destinatario==$authuser)
-                <div>
-                  <p>{!! $messaggio->testo !!}</p>
+                <div class="ricevuto">
+                  <p>{!! $messaggio->testo !!}</p> 
                   <span class="time_date">{{ $messaggio->dataOraInvio }}</span>
                 </div>
                 @else
-                <div>
-                    <right><p>{!! $messaggio->testo !!}</p></right>
-                    <span class="time_date">{{ $messaggio->dataOraInvio }}</span>
+                <div class="inviato">
+                    <p>{!! $messaggio->testo !!}</p>
+                    <span>{{ $messaggio->dataOraInvio }}</span>
                 </div>
+            
                 
                 @endif
-                
+                <br><br>    
             @endforeach
+           
+    </div>
+    <br><br>
+            
 
     
     <div class="type_msg">
@@ -43,6 +81,7 @@
                         @endforeach
                     </div>
                 @endif
+                
                 </div>
                 <div>
                 {{ Form::submit('Invia', ['id'=>'send-message', 'class'=> 'ourblue']) }}
@@ -52,6 +91,9 @@
             </div>
               {{ Form::close() }}
     </div>
+</div>
+
+
 @endisset
 </body>
 </html>
